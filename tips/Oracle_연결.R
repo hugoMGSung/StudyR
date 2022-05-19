d3efbdf4acf04bb74dca107aab12cf8dbb22cece
hugoMGSung <personar95@naver.com>
2022-05-19 17:30:44 +0900

    220519 목요일 커밋!!!

--- tips/Oracle_연결.R
+++ tips/Oracle_연결.R
@@ -3,7 +3,8 @@ library(RODBC)
 
 ## DB 연결정보
 # ODBC 데이터원본(64비트)에 Scott_DSN 추가
-conn1 = odbcConnect('Scott_DSN', uid='scott', pwd='tiger', believeNRows=F)
+conn1 = odbcConnect('Scott_DSN', uid='scott', pwd='tiger', 
+                    believeNRows=F, DBMSencoding = 'euc-kr')
 summary(conn1)
 
 # Run Query
